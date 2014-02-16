4e9482b1a122adb6abe7a6eb1ac270b783cc41b5
Tobias Roeser <le.petit.fou@web.de>
2014-02-16 22:03:00 +0100

    Added documentation and two new properties (deps, phonyTarget).

--- org.sbuild.plugins.jar/src/main/scala/org/sbuild/plugins/jar/Jar.scala
+++ org.sbuild.plugins.jar/src/main/scala/org/sbuild/plugins/jar/Jar.scala
@@ -8,9 +8,16 @@ import de.tototec.sbuild._
  *
  * Based on it's configuration, this plugin will register various targets.
  *
+ * @param jarFile The JAR file which will be created.
+ * @param fileSets The `[[JarFileSets]]` denoting the content of the JAR file.
+ * @param manifest Map of entries, which should be added to the JAR manifest.
+ * @param deps Additional dependencies, which the resulting target should depend on (see `[[de.tototec.sbuild.Target#dependsOn]]`.
+ * @param phonyTarget Name of an optional phony target, which also triggers the generation of the JAR.
  */
 case class Jar(
   jarFile: File,
   fileSets: Seq[JarFileSet],
-  manifest: Map[String, String])
+  manifest: Map[String, String],
+  deps: TargetRefs = TargetRefs(),
+  phonyTarget: Option[String] = None)
 
--- org.sbuild.plugins.jar/src/main/scala/org/sbuild/plugins/jar/JarPlugin.scala
+++ org.sbuild.plugins.jar/src/main/scala/org/sbuild/plugins/jar/JarPlugin.scala
@@ -10,36 +10,44 @@ class JarPlugin(implicit project: Project) extends Plugin[Jar] {
 
   override def create(name: String): Jar = {
     val targetDir = Path("target")
-    val (jar, fileSets) = name match {
+    val (jar, fileSets, phonyTarget) = name match {
       case "" | "main" =>
         (
           targetDir / "main.jar",
           Seq(
             JarFileSet.Dir(targetDir / "classes"),
             JarFileSet.Dir(Path("src/main/resources"))
-          )
+          ),
+            Some("jar")
         )
       case "test" =>
         (
           targetDir / "test.jar",
           Seq(JarFileSet.Dir(targetDir / "test-classes"),
             JarFileSet.Dir(Path("src/test/resources"))
-          )
+          ),
+            None
         )
-      case x => (targetDir / s"${x}.jar", Seq())
+      case x =>
+        (
+          targetDir / s"${x}.jar",
+          Seq(),
+          None
+        )
     }
 
     Jar(
       jarFile = jar,
       fileSets = fileSets,
-      manifest = Map()
+      manifest = Map(),
+      phonyTarget = phonyTarget
     )
   }
 
   override def applyToProject(instances: Seq[(String, Jar)]): Unit = instances foreach {
     case (name, jar) =>
 
-      Target(jar.jarFile) dependsOn jar.fileSets.foldLeft(TargetRefs())((l, r) => l ~ r.targetRefs) exec {
+      val jarTarget = Target(jar.jarFile) dependsOn jar.deps ~~ jar.fileSets.foldLeft(TargetRefs())((l, r) => l ~ r.targetRefs) exec {
         new JarCreator().createJar(
           jarFile = jar.jarFile,
           fileSets = jar.fileSets.map { fs =>
@@ -49,6 +57,10 @@ class JarPlugin(implicit project: Project) extends Plugin[Jar] {
             case Success(_) =>
             case Failure(e) => throw e
           }
+      }
+
+      jar.phonyTarget.map { tName =>
+        Target(s"phony:${tName}") dependsOn jarTarget
       }
 
   }
