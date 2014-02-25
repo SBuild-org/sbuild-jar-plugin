e376f426104c4b774459c9e17abc659c65dc8ecd
Tobias Roeser <le.petit.fou@web.de>
2014-02-25 14:55:19 +0100

    Add dir entries to jar.

--- org.sbuild.plugins.jar/src/main/scala/org/sbuild/plugins/jar/internal/JarCreator.scala
+++ org.sbuild.plugins.jar/src/main/scala/org/sbuild/plugins/jar/internal/JarCreator.scala
@@ -37,31 +37,54 @@ class JarCreator {
     //    val createDirNodes = true
     //    var createdDirNodes: Seq[String] = Seq()
 
-    val entries: Seq[(JarEntry, Option[File])] = fileSets.flatMap {
-      case (baseDir, files) =>
-        val base = baseDir.getPath
-        val index = base.length + 1 /* separator */
-        files.flatMap { file =>
-          val filePath = file.getPath
-          if (!file.exists) {
-            throw new IllegalArgumentException(s"""File "${filePath}" does not exist.""")
-          }
-          if (!filePath.startsWith(base)) {
-            throw new IllegalArgumentException(s"""File "${filePath}" is not located under "${base}"""")
-          }
-          val nodePath = filePath.substring(index)
+    val entries: Seq[(JarEntry, Option[File])] = {
+      var createdDirs: Set[String] = Set()
+      (new JarEntry("META-INF/") -> None) +: fileSets.flatMap {
+        case (baseDir, files) =>
+          val base = baseDir.getPath
+          val index = base.length + 1 /* separator */
+          files.flatMap { file =>
+            val filePath = file.getPath
+            if (!file.exists) {
+              throw new IllegalArgumentException(s"""File "${filePath}" does not exist.""")
+            }
+            if (!filePath.startsWith(base)) {
+              throw new IllegalArgumentException(s"""File "${filePath}" is not located under "${base}"""")
+            }
 
-          //        if(createDirNodes) {
-          //          file.getParentFile()
-          //          
-          //          
-          //        }
+            val nodePath = filePath.substring(index)
 
-          val entry = new JarEntry(nodePath)
-          entry.setTime(file.lastModified)
-          val streamable = if (file.isDirectory) None else Some(file)
-          Seq(entry -> streamable)
-        }
+            def findParents(relFile: File, trace: List[File] = Nil): List[File] = relFile.getParentFile match {
+              case null => trace
+              case p => findParents(p, p :: trace)
+            }
+            val parents = findParents(new File(nodePath))
+
+            val parentEntries = parents.flatMap { dir =>
+              val path = dir.getPath match {
+                case p if p.endsWith("/") => p
+                case p => p + "/"
+              }
+              if (createdDirs.contains(path)) Seq() else {
+                createdDirs += path
+                Seq(new JarEntry(path) -> None)
+              }
+            }
+
+            parentEntries ++ (if (file.isDirectory()) {
+              Seq(new JarEntry(nodePath match {
+                case p if p.endsWith("/") => p
+                case p => p + "/"
+              }) -> None)
+            } else {
+              val entry = new JarEntry(nodePath)
+              entry.setTime(file.lastModified)
+              val streamable = if (file.isDirectory) None else Some(file)
+              Seq(entry -> streamable)
+            })
+
+          }
+      }
     }
 
     val explicitManifests = entries.filter { case (entry, _) => entry == "META-INF/MANIFEST.MF" }
